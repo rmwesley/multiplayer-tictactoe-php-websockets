84c22f7ea11c1195c78914eac22e7f49cfb769bd
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-17 07:26:22 +0200

    Implemented the Room class in PHP
    This ia a template for a game room, containing its data and behavior

--- ws_server.php
+++ ws_server.php
@@ -1,41 +1,5 @@
 <?php
 
-function getWinner($board, $turn){
-	if($turn < 3) return;
-	$winner = checkVerticals($board);
-	$winner = checkHorizontals($board) ?? $winner;
-	$winner = checkDiagonals($board) ?? $winner;
-	return $winner;
-}
-function checkHorizontals($board){
-	for ($i=0; $i<3; $i++) {
-		$winner = $board[3*$i];
-		if($winner == "_") continue;
-		if($board[3*$i+1] == $winner && $board[3*$i+2] == $winner){
-			return $winner;
-		}
-	}
-}
-function checkVerticals($board){
-	for ($j=0; $j<3; $j++) {
-		$winner = $board[$j];
-		if($winner == "_") continue;
-		if($board[$j+3] == $winner && $board[$j+6] == $winner){
-			return $winner;
-		}
-	}
-}
-function checkDiagonals($board){
-	$winner = $board[4];
-	if($winner == "_") return;
-	if($board[0] == $winner && $board[8] == $winner) return $winner;
-	if($board[2] == $winner && $board[6] == $winner) return $winner;
-}
-
-function checkMove($move, $boardMarkings){
-	return $boardMarkings[$move] == "_";
-}
-
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
@@ -45,7 +9,133 @@ use React\EventLoop\Factory;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
 
-class TicTacToe implements Ratchet\MessageComponentInterface {
+class Room {
+	private $roomId;
+	private $board = "_________";
+	private $turn = 1;
+	private $player1;
+	private $player2;
+	private $xPlayerNumber;
+
+	function linkPlayers(){
+		$this->player1->roomId = $this->roomId;
+		$this->player2->roomId = $this->roomId;
+	}
+	function __construct($player1, $player2, $roomId){
+		$this->player1 = $player1;
+		$this->player2 = $player2;
+		$this->roomId = $roomId;
+		$this->xPlayerNumber = rand(1,2);
+
+		$this->linkPlayers();
+		// Setting up symbol for player1 player randomly
+		//$this->firstSymbol = array("X", "O")[rand(0, 1)];
+	}
+
+	function getPlayer1(){
+		return $this->player1;
+	}
+	function getPlayer2(){
+		return $this->player2;
+	}
+	function getXPlayerNumber(){
+		return $this->xPlayerNumber;
+	}
+	function getTurn(){
+		return $this->turn;
+	}
+	function getBoard(){
+		return $this->board;
+	}
+	function updatePlayerConnection($from){
+		if ($from->username == $this->player1->username) {
+			$this->player1 = $from;
+		}
+		else $this->player2 = $from;
+	}
+	function isInRoom($from){
+		if($from->username == $this->player1->username
+			|| $from->username == $this->player2->username){
+				return true;
+			}
+		return false;
+	}
+	function getCurrentPlayer(){
+		// Odd turn
+		if($this->getTurn()%2 == 1){
+			return $this->player1;
+		}
+		return $this->player2;
+	}
+	function currentIsX(){
+		// Turn is odd
+		if($this->getTurn()%2 == 1){
+			// Is odd player X?
+			return $this->xPlayerNumber == 1;
+		}
+		// Turn is even
+		// Is even player X?
+		return $this->xPlayerNumber == 2;
+	}
+	function getCurrentSymbol(){
+		if ($this->currentIsX()){
+			return 'X';
+		}
+		return 'O';
+	}
+	function nextMove($move){
+		//// Flip the turn, 0 to 1, 1 to 0 with XOR
+		//$room->turn = $room->turn ^ 1;
+		$this->turn++;
+		$this->board[$move] = $this->getCurrentSymbol();
+	}
+
+	function getWinner(){
+		//if($turn < 3) return;
+		//$winner = checkVerticals($board);
+		//$winner = checkHorizontals($board) ?? $winner;
+		//$winner = checkDiagonals($board) ?? $winner;
+		if($this->getTurn() < 3) return;
+		$winner = $this->checkVerticals();
+		$winner = $this->checkHorizontals() ?? $winner;
+		$winner = $this->checkDiagonals() ?? $winner;
+		return $winner;
+	}
+	function gameOver(){
+		if($this->turn > 9) return true;
+		if($this->getWinner() != null) return true;
+	}
+	function checkHorizontals(){
+		for ($i=0; $i<3; $i++) {
+			$winner = $this->board[3*$i];
+			if($winner == "_") continue;
+			if($this->board[3*$i+1] == $winner && $this->board[3*$i+2] == $winner){
+				return $winner;
+			}
+		}
+	}
+	function checkVerticals(){
+		for ($j=0; $j<3; $j++) {
+			$winner = $this->board[$j];
+			if($winner == "_") continue;
+			if($this->board[$j+3] == $winner && $this->board[$j+6] == $winner){
+				return $winner;
+			}
+		}
+	}
+	function checkDiagonals(){
+		$winner = $this->board[4];
+		if($winner == "_") return;
+		if($this->board[0] == $winner && $this->board[8] == $winner) return $winner;
+		if($this->board[2] == $winner && $this->board[6] == $winner) return $winner;
+	}
+
+	function checkAvailability($move){
+		return $this->board[$move] == "_";
+	}
+}
+
+class WsHandler implements Ratchet\MessageComponentInterface {
 	private $queue;
 	private $queueCounter;
 	private $connectionsMap;
