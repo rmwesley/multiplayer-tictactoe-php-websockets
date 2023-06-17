7a8cabe04a9979bf85b8b65e9edd7911b3479100
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-17 05:19:01 +0200

    Renamed file to home.js and added input validation

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,12 @@
 const loginForm = document.querySelector('#loginForm');
 const registerForm = document.querySelector('#registerForm');
 
+// Password and Confirmed password validation
+function validateForm(){
+	let passwordList = registerForm.querySelectorAll("input[type='password']");
+	if(passwordList[0].value == passwordList[1].value) return true;
+
+}
 function showLoginForm() {
 	loginForm.classList.remove('d-none');
 	registerForm.classList.add('d-none');
