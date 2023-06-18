ac1cd54cd10ba46957ca2779c64bf85ec3d1f519
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-18 21:07:37 +0200

    Reidented api/ code with 4 spaces instead of tabs

--- api/login.php
+++ api/login.php
@@ -3,29 +3,29 @@ session_start();
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	// Opening database connection
-	require_once '../config/db.php';
+    // Opening database connection
+    require_once '../config/db.php';
 
-	$username = mysqli_real_escape_string($conn, $_POST['username']);
-	$password = mysqli_real_escape_string($conn, $_POST['password']);
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-	$sql = "SELECT * FROM users WHERE username = '$username'";
-	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) == 0) {
-		header("location:../index.php?loginFailed=true&reason=username");
-		exit;
-	}
-	$row = mysqli_fetch_assoc($result);
-	if (!password_verify($password, $row['hash'])) {
-		$_SESSION['error'] = "Password is incorrect.";
-		header("location:../index.php?loginFailed=true&reason=password");
-		exit;
-	}
-	$_SESSION['username'] = $username;
-	header("Location: ../index.php?loginSuccess=true");
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) == 0) {
+        header("location:../index.php?loginFailed=true&reason=username");
+        exit;
+    }
+    $row = mysqli_fetch_assoc($result);
+    if (!password_verify($password, $row['hash'])) {
+        $_SESSION['error'] = "Password is incorrect.";
+        header("location:../index.php?loginFailed=true&reason=password");
+        exit;
+    }
+    $_SESSION['username'] = $username;
+    header("Location: ../index.php?loginSuccess=true");
 
-	// Closing database connection
-	mysqli_close($conn);
+    // Closing database connection
+    mysqli_close($conn);
 }
 
 ?>
