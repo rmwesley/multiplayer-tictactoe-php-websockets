f2957e29359f2b75e552b0391d017d0699443023
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-19 12:07:35 +0200

    Removed game.php, only index.php at root now
    Refactored index.php code to handle all GET requests,
    be them for going to the lobby or to the game room.

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php
 session_start();
 
-// Check if user is already logged in
-if (isset($_SESSION['username'])) {
-    $username = $_SESSION['username'];
-    $html = file_get_contents("views/lobby.html");
-    $html = str_replace("{{username}}", $username, $html);
-    echo $html;
-    exit;
+// Check if user is not yet logged in
+if (empty($_SESSION['username'])) {
+	// Display the registration/login form
+	include "views/home.html";
+	exit;
 }
 
+$username = $_SESSION['username'];
 
+// Check if user is not yet in a room
+if(empty($_GET['room_id'])){
+	$html = file_get_contents("views/lobby.html");
+	$html = str_replace("{{username}}", $username, $html);
+	echo $html;
+	exit;
+}
 
-// Display the registration/login form
-include "views/home.html";
+$room_id = $_GET['room_id'];
+
+$html = file_get_contents("views/game.html");
+$html = str_replace("{{username}}", $username, $html);
+$html = str_replace("{{roomId}}", $room_id, $html);
+echo $html;
+?>
