rmwesley/tictactoe-server-game

Index, Login and Registration pages implemented

login.php
@@ -1,0 +1,37 @@
+<?php
+// connect to database
+$servername = "localhost";
+$dbusername = "tictac_adm";
+$dbpassword = "4chan>>>redditLOL";
+$dbname = "tictactoe";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            header("Location: redirection.html");
+            exit;
+        } else {
+            echo "Password is incorrect.";
+        }
+    } else {
+        echo "Username does not exist.";
+    }
+}
+
+mysqli_close($conn);
+?>

register.php
@@ -1,0 +1,39 @@
+<?php
+// connect to database
+$servername = "localhost";
+$dbusername = "tictac_adm";
+$dbpassword = "4chan>>>redditLOL";
+$dbname = "tictactoe";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirmPassword']);
+
+    // Check if both passwords match
+    if ($password == $confirmPassword) {
+        // Hash password before saving
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    } else {
+        echo "Passwords do not match.";
+    }
+}
+
+mysqli_close($conn);
+?>

rmwesley/multiplayer-tictactoe-php-websockets

login.php
@@ -1,0 +1,37 @@
+<?php
+// connect to database
+$servername = "hostname";
+$dbusername = "dbuser";
+$dbpassword = "dbpass";
+$dbname = "dbname";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            header("Location: redirection.html");
+            exit;
+        } else {
+            echo "Password is incorrect.";
+        }
+    } else {
+        echo "Username does not exist.";
+    }
+}
+
+mysqli_close($conn);
+?>

register.php
@@ -1,0 +1,39 @@
+<?php
+// connect to database
+$servername = "hostname";
+$dbusername = "dbuser";
+$dbpassword = "dbpass";
+$dbname = "dbname";
+
+// Create connection
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirmPassword']);
+
+    // Check if both passwords match
+    if ($password == $confirmPassword) {
+        // Hash password before saving
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    } else {
+        echo "Passwords do not match.";
+    }
+}
+
+mysqli_close($conn);
+?>