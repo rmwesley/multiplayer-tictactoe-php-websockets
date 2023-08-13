0c97cb9dc102d52e8c560846306cbc49012f27d3
Wesley <wesley.machado.2018@polytechnique.org>
2023-08-14 01:31:52 +0200

    Added bootstrap's "Grip vertical" icon to chatbox
    This is meant to improve User Experience.
    The grip icon makes users realize the chatbox is draggable.

--- htdocs/api/content_chatbox.php
+++ htdocs/api/content_chatbox.php
@@ -2,6 +2,9 @@
 $chatbox = <<< HERE
 <div id="chatbox" class="border bg-body">
   <div id="chatbox-header" class="border">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi" viewBox="0 0 16 16">
+      <path d="M7 2a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 5a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-3 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
+    </svg>
     Chat
   </div>
 
