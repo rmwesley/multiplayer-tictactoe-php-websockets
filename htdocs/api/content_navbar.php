9a09446ffed48e7aa0783ed82b909299ce9ea28a
Wesley <wesley.machado.2018@polytechnique.org>
2023-08-17 16:52:52 +0200

    TOC is now kept in offcanvas instead of dropdown

--- htdocs/api/content_navbar.php
+++ htdocs/api/content_navbar.php
@@ -193,30 +193,16 @@ $icons = <<< HERE
   </symbol>
 </svg>
 HERE;
+
 // Finally setting up navbar
 $navbar = <<< HERE
 <nav class='navbar mb-1 navbar-expand-md navbar-dark static-top bg-dark'>
   <div class='container-fluid'>
     <a class='navbar-brand' href='/'>TicTacToe</a>
-    <button class='me-auto navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>
+    <button class='navbar-toggler border-0 me-auto' type='button' data-bs-toggle='offcanvas' data-bs-target='#tableOfContents' aria-label='TOC'>
       <span class='navbar-toggler-icon'></span>
     </button>
-    <div class='collapse navbar-collapse' id='navbarCollapse'>
-      <ul class='navbar-nav me-auto mb-2 mb-md-0'>
-        <li class='nav-item'>
-          <a class='nav-link' href='?page=lobby'>Lobby</a>
-        </li>
-        <li class='nav-item'>
-          <a class='nav-link' href='?page=game'>Game Room</a>
-        </li>
-        <li class='nav-item'>
-          <a class='nav-link' href='?page=history'>Match history</a>
-        </li>
-        <li class='nav-item'>
-          <a class='nav-link' href='?page=about'>About page</a>
-        </li>
-      </ul>
-    </div>
+    {{table_of_contents}}
     {{profile_button}}
     {{theme_button}}
   </div>
@@ -225,6 +211,32 @@ $navbar = <<< HERE
 {{auth_offcanvas}}
 HERE;
 
+$tocOffcanvas = <<< HERE
+<div class='offcanvas offcanvas-start' id='tableOfContents'>
+  <div class="offcanvas-header border-bottom">
+    <h5 class="offcanvas-title" id="bdSidebarOffcanvasLabel">Browse pages</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#bdSidebar"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class='navbar-nav me-auto mb-2 mb-md-0'>
+      <li class='nav-item'>
+        <a class='nav-link' href='?page=lobby'>Lobby</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='?page=game'>Game Room</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='?page=history'>Match history</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link' href='?page=about'>About page</a>
+      </li>
+    </ul>
+  </div>
+</div>
+HERE;
+
+$navbar = str_replace("{{table_of_contents}}", $tocOffcanvas, $navbar);
 $navbar = str_replace("{{auth_offcanvas}}", $authOffcanvas, $navbar);
 $navbar = str_replace("{{profile_button}}", $profileButton, $navbar);
 $navbar = str_replace("{{theme_button}}", $themeButton, $navbar);
